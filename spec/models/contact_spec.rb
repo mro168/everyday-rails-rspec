2d4df55dfe009f9f38b2c822fffd85fcca556c34
Mario Stefano <me@mario.sg>
2014-06-29 18:08:02 +0700

    4. Generating test data with factories

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,66 +2,52 @@ require 'spec_helper'
 
 describe Contact do
 
-  it 'is valid with a firstname, lastname, and email' do
-    contact = Contact.new(
-      firstname: 'Mario Stefano',
-      lastname: 'Suharja',
-      email: 'tester@example.com'
-    )
-    expect(contact).to be_valid
+  it 'is has a valid factory' do
+    expect(build(:contact)).to be_valid
   end
 
   it 'is invalid without a firstname' do
-    expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
+    contact = build(:contact, firstname: nil)
+    expect(contact).to have(1).errors_on(:firstname)
   end
 
   it 'is invalid without a lastname' do
-    expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+    contact = build(:contact, lastname: nil)
+    expect(contact).to have(1).errors_on(:lastname)
   end
 
   it 'is invalid without an email address' do
-    expect(Contact.new(email: nil)).to have(1).errors_on(:email)
+    contact = build(:contact, email: nil)
+    expect(contact).to have(1).errors_on(:email)
   end
 
   it 'is invalid with a duplicate email address' do
-    Contact.create(
-      firstname: 'Joe',
-      lastname: 'Tester',
-      email: 'tester@example.com'
-    )
-    contact = Contact.new(
-      firstname: 'Jane',
-      lastname: 'Tester',
-      email: 'tester@example.com'
-    )
+    create( :contact, email: 'tester@example.com' )
+    contact = build(  :contact, email: 'tester@example.com' )
     expect(contact).to have(1).errors_on(:email)
   end
 
   it 'returns a contact\'s full name as a string' do
-    contact = Contact.new(
+    contact = build(:contact,
       firstname: 'John',
-      lastname: 'Doe',
-      email: 'johndoe@example.com'
+      lastname: 'Doe'
     )
     expect(contact.name).to eq 'John Doe'
   end
 
   describe 'filter last name by letter' do
     before :each do
-      @smith = Contact.create(
+      @smith = create(:contact,
         firstname: 'John',
-        lastname: 'Smith',
-        email: 'jsmith@example.com'
+        lastname: 'Smith'
       )
-      @jones = Contact.create(
+      @jones = create(:contact,
         firstname: 'Tim',
-        lastname: 'Jones',
-        email: 'tjones@example.com'
+        lastname: 'Jones'
       )
-      @johnson = Contact.create(
+      @johnson = create(:contact,
         firstname: 'John',
-        lastname: 'Johnson',
-        email: 'jjohnson@example.com'
+        lastname: 'Johnson'
       )
     end
 
@@ -76,6 +62,10 @@ describe Contact do
         expect(Contact.by_letter('J')).to_not include @smith
       end
     end
+  end
+
+  it 'has three phone numbers' do
+    expect(create(:contact).phones.count).to eq 3
   end
 
 end
