8539089abee96e132634f9acf4d7a0c66919f2f2
Mario Stefano <me@mario.sg>
2014-07-01 11:12:18 +0700

    6. Controller spec cleanup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -42,4 +42,7 @@ RSpec.configure do |config|
 
   # Include Factory Girl syntax to simplify calls to factories
   config.include FactoryGirl::Syntax::Methods
+
+  # Include LoginMacros support
+  config.include LoginMacros
 end
